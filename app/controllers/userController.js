837ac8931fc5bf2cff65a1413da35cfb0a677a9d
sgomez <sergiogomezvictoria@gmail.com>
2015-10-12 11:09:33 -0500

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app/app.css
    
    User list fixed

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -9,14 +9,40 @@
         $scope.projects = [];
 
         $scope.getUser = function(){
+
+            var loader = document.getElementById("user-loader");
+            var userInfo = document.getElementById("user-info");
+            loader.className = "";
+            userInfo.className = "user-info";
+
             userService.getUser($scope.username).then(function(data){
-                $scope.users = data;
+                loader.className = "user-loader";
+                userInfo.className = "";
+                $scope.users = data.items;
+                console.log($scope.users);
+            }).catch(function(response){
+                loader.className = "user-loader";
+                console.log(response);
             });
         };
 
         $scope.getProjects = function(user) {
+
+            var loader = document.getElementById("project-loader");
+            var projectInfo = document.getElementById("project-info");
+            loader.className = "";
+            projectInfo.className = "";
+
             userService.getProjects(user.login).then(function(data){
+                loader.className = "project-loader";
+                projectInfo.className = "popup-wrapper";
                 $scope.projects = data;
+                console.log($scope.projects);
+            }).catch(function (reject) {
+                alert("Not available");
+                loader.className = "project-loader";
+                console.log(reject);
+                return null;
             });
         };
     }]);
--- app/services/userService.js
+++ app/services/userService.js
@@ -6,39 +6,26 @@
        .factory('userService', ['$http', '$cacheFactory', function($http, $cacheFactory) {
 
             function getUser(username) {
-                var loader = document.getElementById("user-loader");
-                var userInfo = document.getElementById("user-info");
-                loader.className = "";
-                userInfo.className = "user-info";
 
                 return $http.get('https://api.github.com/search/users?q=' + username, {cache:true})
                     .then(function (response) {
-                        loader.className = "user-loader";
-                        userInfo.className = "";
+                        var users = response.data;
                         return users;
                     }).catch(function (response) {
                         alert("User not found");
-                        loader.className = "user-loader";
                         console.log(response);
                         return response.statusText;
                     });
             }
 
             function getProjects(username) {
-                var loader = document.getElementById("project-loader");
-                var projectInfo = document.getElementById("project-info");
-                loader.className = "";
-                projectInfo.className = "project-info";
                 var urlRepos = 'https://api.github.com/users/' + username + '/repos';
                 return $http.get(urlRepos, {cache:true})
                     .then(function (response) {
-                        loader.className = "project-loader";
-                        projectInfo.className = "";
-                        user.projects = response.data;
-                        return user.projects;
+                        var projects = response.data;
+                        return projects;
                     }).catch(function (reject) {
                         alert("Not available");
-                        loader.className = "project-loader";
                         console.log(reject);
                         return null;
                     }
